292c84291295c2c0043c075ab8d6eb7f9a264e39
hhhhungc <dial0122@gmail.com>
2024-07-19 23:50:08 +0800

    add components/layout

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Layout from '../../components/layout'
 
 export default function Blog() {
   return (
     <>
-      <h1>Blog</h1>
-
-      <Link href="/">Back to home</Link>
-      <Image src="/images/profile.jpg" height={144} width={144} alt="test" />
+      <Layout>
+        <h1>Blog</h1>
+        <Image src="/images/profile.jpg" height={144} width={144} alt="test" />
+        <Link href="/">Back to home</Link>
+      </Layout>
     </>
   )
 }
