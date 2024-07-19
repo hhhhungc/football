fdec4790e88cf05195e3644b2c923337e42ff845
hhhhungc <dial0122@gmail.com>
2024-07-19 23:30:13 +0800

    add metadata

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Hello</title>
+      </Head>
       <h1>Hello Next!</h1>
     </>
   )
